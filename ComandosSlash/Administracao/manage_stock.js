d73be21b68ce5f4d122fade8fa3fd8a234acbcae
Replitomelhor <replitprogrammer@proton.me>
2026-04-20 16:09:15 -0300

    Add ComandosSlash/Administracao/manage_stock.js

---
+++ ComandosSlash/Administracao/manage_stock.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, ApplicationCommandType, ActionRowBuilder, ButtonBuilder } = require("discord.js");
+const { MessageStock } = require("../../Functions/ConfigEstoque.js");
+const { getPermissions } = require("../../Functions/PermissionsCache.js");
+const { owner } = require("../../config.json")
+
+module.exports = {
+  name: "manage_stock",
+  description: "Use para configurar minhas funções",
+  type: ApplicationCommandType.ChatInput,
+  options: [{ name: "item", description: "-", type: 3, required: true, autocomplete: true }],
+
+  run: async (client, interaction, message) => {
+
+    if (interaction.user.id !== owner) {
+
+      interaction.reply({ content: `❌ | Você não pode usar este comando!`, ephemeral: true });
+      return;
+
+    }
+
+    if (interaction.options._hoistedOptions[0].value == 'nada') return interaction.reply({ content: `Nenhum item registrado em seu BOT`, ephemeral: true })
+
+
+    const separarpor_ = interaction.options._hoistedOptions[0].value.split('_')
+    const produtoname = separarpor_[0]
+    const camponame = separarpor_[1]
+
+    MessageStock(interaction, 1, produtoname, camponame)
+
+  }
+}
